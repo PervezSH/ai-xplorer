fb492bb0c000221d99c26d7df4776250e394e0b9
PervezSH <50441829+PervezSH@users.noreply.github.com>
2023-01-07 12:57:19 +0530

    feat: add api key container ✨

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,51 @@
 import headerStyles from '../styles/components/Header.module.scss';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 const Header = () => {
+    const [apiKey, setApiKey] = useState<string>("");
+    const [keyEntered, setKeyEntered] = useState<boolean>(false);
+    const [inUpdateMode, setInUpdateMode] = useState<boolean>(true);
+
     return (
         <div className={headerStyles.container}>
             <div>Ai Xplorer</div>
-            <div>Login</div>
+            <div className={headerStyles["api-key-container"]}>
+                <button
+                    className={headerStyles["api-key-button"]}
+                    onClick={() => {
+                        apiKey.length > 0 ? setKeyEntered(true) : setKeyEntered(false)
+                        setInUpdateMode(true);
+                    }}
+                >
+                    <Image src="/images/key.png" alt="Key" width={20} height={20} />
+                    <p>{(keyEntered && apiKey.length > 0) ? `${apiKey}` : `Add API Key`}</p>
+                </button>
+                {(inUpdateMode && keyEntered) && (
+                    <div>
+                        <p>You already entered you OpenAI API key</p>
+                        <button onClick={() => setKeyEntered(false)}>Change Key</button>
+                    </div>
+                )}
+                {(inUpdateMode && !keyEntered) && (
+                    <div>
+                        <p>To get started, add your OpenAI API Key!</p>
+                        <input
+                            type="text"
+                            placeholder="Enter here..."
+                            value={apiKey}
+                            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                                setApiKey(event.target.value);
+                            }}
+                        />
+                        <button onClick={() => {
+                            if (apiKey.length === 0) return;
+                            setKeyEntered(true);
+                            setInUpdateMode(false);
+                        }}>Add key</button>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
