9bfef8a3e15b15256f96d2bc14a952379aca4f27
PervezSH <50441829+PervezSH@users.noreply.github.com>
2023-01-09 15:16:03 +0530

    chore: gpt3-models api ♻️

--- pages/api/gpt3-models/index.ts
+++ pages/api/gpt3-models/index.ts
@@ -1,15 +1,15 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { gpt3Models } from '../../../utils/model-data'
+import { NextApiRequest, NextApiResponse } from 'next';
+import { gpt3Models } from '../../../utils/model-data';
 
 const handler = (_req: NextApiRequest, res: NextApiResponse) => {
     try {
         if (!Array.isArray(gpt3Models)) {
-            throw new Error('Cannot find gpt3 models')
+            throw new Error('Cannot find gpt3 models');
         }
-        res.status(200).json(gpt3Models)
+        res.status(200).json(gpt3Models);
     } catch (err: any) {
-        res.status(500).json({ statusCode: 500, message: err.message })
+        res.status(500).json({ statusCode: 500, message: err.message });
     }
-}
+};
 
-export default handler
+export default handler;
