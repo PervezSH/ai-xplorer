d6aca8d97cc7aa1967f28c601a3941f616a80ef4
PervezSH <50441829+PervezSH@users.noreply.github.com>
2023-01-18 15:45:11 +0530

    style: header component 💅

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,54 +1,77 @@
-import headerStyles from '../styles/components/Header.module.scss';
-import Image from 'next/image';
-import { useState } from 'react';
-import { useApiKey } from '../context/ApiKeyContext';
+import headerStyles from "../styles/components/Header.module.scss";
+import Image from "next/image";
+import { useState } from "react";
+import { useApiKey } from "../context/ApiKeyContext";
 
 const Header = () => {
-    const { apiKey, setApiKey } = useApiKey();
-    const [keyEntered, setKeyEntered] = useState<boolean>(apiKey.length > 0 ? true : false);
-    const [inUpdateMode, setInUpdateMode] = useState<boolean>(apiKey.length > 0 ? false : true);
+  const { apiKey, setApiKey } = useApiKey();
+  const [keyEntered, setKeyEntered] = useState<boolean>(
+    apiKey.length > 0 ? true : false
+  );
+  const [inUpdateMode, setInUpdateMode] = useState<boolean>(
+    apiKey.length > 0 ? false : true
+  );
 
-    return (
-        <div className={headerStyles.container}>
-            <div>Ai Xplorer</div>
-            <div className={headerStyles["api-key-container"]}>
-                <button
-                    className={headerStyles["api-key-button"]}
-                    onClick={() => {
-                        apiKey.length > 0 ? setKeyEntered(true) : setKeyEntered(false)
-                        setInUpdateMode(true);
-                    }}
-                >
-                    <Image src="/images/key.png" alt="Key" width={20} height={20} />
-                    <p>{(keyEntered && apiKey.length > 0) ? `${apiKey}` : `Add API Key`}</p>
-                </button>
-                {(inUpdateMode && keyEntered) && (
-                    <div>
-                        <p>You already entered you OpenAI API key</p>
-                        <button onClick={() => setKeyEntered(false)}>Change Key</button>
-                    </div>
-                )}
-                {(inUpdateMode && !keyEntered) && (
-                    <div>
-                        <p>To get started, add your OpenAI API Key!</p>
-                        <input
-                            type="text"
-                            placeholder="Enter here..."
-                            value={apiKey}
-                            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                                setApiKey(event.target.value);
-                            }}
-                        />
-                        <button onClick={() => {
-                            if (apiKey.length === 0) return;
-                            setKeyEntered(true);
-                            setInUpdateMode(false);
-                        }}>Add key</button>
-                    </div>
-                )}
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className={headerStyles.container}>
+      <Image src="/images/logo.svg" alt="Logo" width={109} height={40} />
+      <div className={headerStyles["api-key-container"]}>
+        <button
+          className={headerStyles["api-key-button"]}
+          onClick={() => {
+            apiKey.length > 0 ? setKeyEntered(true) : setKeyEntered(false);
+            setInUpdateMode(true);
+          }}
+        >
+          <Image src="/images/key.svg" alt="Key" width={20} height={20} />
+          <p>
+            {keyEntered && apiKey.length > 0
+              ? `${
+                  apiKey.substring(0, 3) +
+                  "..." +
+                  apiKey.substring(apiKey.length - 4, apiKey.length)
+                }`
+              : `Add API Key`}
+          </p>
+        </button>
+        {inUpdateMode && keyEntered && (
+          <div>
+            <p>You already entered you OpenAI API key</p>
+            <button
+              className={headerStyles["btn-secondary"]}
+              onClick={() => setKeyEntered(false)}
+            >
+              Change Key
+            </button>
+          </div>
+        )}
+        {inUpdateMode && !keyEntered && (
+          <div>
+            <p>To get started, add your OpenAI API Key!</p>
+            <input
+              className={headerStyles["in-secondary"]}
+              type="text"
+              placeholder="Enter here..."
+              value={apiKey}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setApiKey(event.target.value);
+              }}
+            />
+            <button
+              className={headerStyles["btn-secondary"]}
+              onClick={() => {
+                if (apiKey.length === 0) return;
+                setKeyEntered(true);
+                setInUpdateMode(false);
+              }}
+            >
+              Add key
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Header;
