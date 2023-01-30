1032de1bef5059e6f474a1ad3d3dc6bc13e10a92
PervezSH <50441829+PervezSH@users.noreply.github.com>
2023-01-30 12:29:01 +0530

    style: footer ✨

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,27 @@
-import footerStyles from '../styles/components/Footer.module.scss';
+import Image from "next/image";
+
+import footerStyles from "../styles/components/Footer.module.scss";
 
 const Footer = () => {
-    return (
-        <div className={footerStyles.container}>
-            <div>Ai Xplorer</div>
-        </div>
-    );
-}
+  return (
+    <div className={footerStyles.container}>
+      <Image
+        src="/images/logo-for-dark.svg"
+        alt="Logo"
+        width={109}
+        height={40}
+      />
+      <div
+        className={footerStyles["twitter-handle"]}
+        onClick={() => {
+          window.open("https://twitter.com/per_0x", "_blank");
+        }}
+      >
+        <Image src="/images/twitter.svg" alt="twitter" width={16} height={16} />
+        <p>@per_0x</p>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
