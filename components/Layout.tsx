da373cc59eeb76b5cfeaedd2a253c5e2fc13ddc8
PervezSH <50441829+PervezSH@users.noreply.github.com>
2023-02-01 15:54:45 +0530

    feat: add meta info ℹ️

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ApiKeyProvider } from "../context/ApiKeyContext";
 import Image from "next/image";
+import Head from "next/head";
 
 import Header from "./Header";
 import Footer from "./Footer";
@@ -8,23 +9,34 @@ import layoutStyles from "../styles/components/Layout.module.scss";
 
 const Layout = ({ children }: any) => {
   return (
-    <ApiKeyProvider>
-      <div className={layoutStyles.container}>
-        <Header />
-        <div className={layoutStyles["bg-illustration-wrapper"]}>
-          <Image
-            className={layoutStyles["bg-illustration"]}
-            src="/images/bg-illustration.svg"
-            alt="bg-illustration"
-            width={1304.43}
-            height={623}
-          />
+    <>
+      <Head>
+        <title>Ai Xplorer</title>
+        <meta
+          name="description"
+          content="A utility tool to Xplore different AI models and pick the right one that is cost-efficient, fast, and effective."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ApiKeyProvider>
+        <div className={layoutStyles.container}>
+          <Header />
+          <div className={layoutStyles["bg-illustration-wrapper"]}>
+            <Image
+              className={layoutStyles["bg-illustration"]}
+              src="/images/bg-illustration.svg"
+              alt="bg-illustration"
+              width={1304.43}
+              height={623}
+            />
+          </div>
+          <main>{children}</main>
+          <div className={layoutStyles.filler} />
+          <Footer />
         </div>
-        <main>{children}</main>
-        <div className={layoutStyles.filler} />
-        <Footer />
-      </div>
-    </ApiKeyProvider>
+      </ApiKeyProvider>
+    </>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,3 @@
-import Head from 'next/head'
-
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Ai Xplorer</title>
-        <meta name="description" content="A utility tool to Xplore different AI models and pick the right one that is cost-efficient, fast, and effective." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </>
-  )
+  return <></>;
 }
